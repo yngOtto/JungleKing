6613953d27ba368b620bba36a21e9b186f372921
Otto <olarsen22@gmail.com>
2023-02-22 18:03:30 +0100

    add player info logic

--- api.py
+++ api.py
@@ -1,7 +1,13 @@
-api_key = ""
+api_key = "RGAPI-535a0fc6-9e31-4109-8fe7-b40992ccfda5"
 
-api_url = ""
+api_url = " https://euw1.api.riotgames.com/lol/summoner/v4/summoners/by-name/otto%20from%20asylum"
 
 import requests
 
 requests.get(api_url)
+
+api_url = api_url + "?api_key=" + api_key
+
+resp = requests.get(api_url)
+
+player_info = resp.json()
