91ede20246168309ba9cd8bd6801cb190fa51e94
Otto <olarsen22@gmail.com>
2023-03-01 23:13:56 +0100

    add summoner info getter

--- api.py
+++ api.py
@@ -51,3 +51,11 @@ def get_summoner_id(summoner_name):
 def get_account_id(summoner_name):
     account_id = player_info["accountId"]
     return account_id
+
+
+def get_summoner_info(summoner_name):
+    api_url = " https://euw1.api.riotgames.com/lol/summoner/v4/summoners/by-name/otto%20from%20asylum"
+    api_url = api_url + "?api_key=" + api_key
+    resp = requests.get(api_url)
+    player_info = resp.json()
+    return player_info
