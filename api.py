1649d83b123cfd781ad2ad585a1494b1e3fb847c
Otto <olarsen22@gmail.com>
2023-03-26 21:11:37 +0200

    refactor methods

--- api.py
+++ api.py
@@ -1,116 +1,58 @@
 import requests
-
 from riotwatcher import LolWatcher, ApiError
+import pandas as pd
 
 lol_watcher = LolWatcher('')
-
 my_region = 'euw1'
 
+# Get the latest champion version and list of champions
 latest = lol_watcher.data_dragon.versions_for_region(my_region)[
     'n']['champion']
-
 static_champ_list = lol_watcher.data_dragon.champions(latest, False, 'en_US')
 
+# Get summoner information
 me = lol_watcher.summoner.by_name(my_region, 'otto from asylum')
 print(me)
-
 my_ranked_stats = lol_watcher.league.by_summoner(my_region, me['id'])
 print(my_ranked_stats)
 
+# Set up the API URL and request headers
 api_key = ""
+api_url = "https://euw1.api.riotgames.com/lol/summoner/v4/summoners/by-name/otto%20from%20asylum"
+s = requests.Session()
+s.headers.update({'X-Riot-Token': api_key})
 
-api_url = " https://euw1.api.riotgames.com/lol/summoner/v4/summoners/by-name/otto%20from%20asylum"
-
-
+# Map the champion IDs to their names
 champ_dict = {}
 for key in static_champ_list['data']:
     row = static_champ_list['data'][key]
     champ_dict[row['key']] = row['id']
+
+# Print the list of participants with champion names
 for row in participants:
     print(str(row['champion']) + ' ' + champ_dict[str(row['champion'])])
     row['championName'] = champ_dict[str(row['champion'])]
 
-# dataframe print
+# Convert lane and role information to a standardized format
 df = pd.DataFrame(participants)
-df
-
-(MID_LANE, SOLO): MIDDLE
-(TOP_LANE, SOLO): TOP
-(BOT_LANE, DUO_CARRY): BOTTOM
-(BOT_LANE, DUO_SUPPORT): UTILITY
-(JUNGLE, NONE): JUNGLE
-
-a = df['lane'] == MID_LANE
-b = df['role'] == SOLO
-c = df['lane'] == TOP_LANE
-d = df['role'] == SOLO
-e = df['lane'] == BOT_LANE
-f = df['role'] == DUO_CARRY
-g = df['lane'] == BOT_LANE
-h = df['role'] == DUO_SUPPORT
-i = df['lane'] == JUNGLE
-j = df['role'] == NONE
-
-for row in df:
-    if a and b:
-        df['lane'] = MIDDLE
-    elif c and d:
-        df['lane'] = TOP
-    elif e and f:
-        df['lane'] = BOTTOM
-    elif g and h:
-        df['lane'] = UTILITY
-    elif i and j:
-        df['lane'] = JUNGLE
-    else:
-        df['lane'] = 'NONE'
-
-
-def get_lane(role, lane):
-    if lane == MID_LANE and role == SOLO:
-        return MIDDLE
-    elif lane == TOP_LANE and role == SOLO:
-        return TOP
-    elif lane == BOT_LANE and role == DUO_CARRY:
-        return BOTTOM
-    elif lane == BOT_LANE and role == DUO_SUPPORT:
-        return UTILITY
-    elif lane == JUNGLE and role == NONE:
-        return JUNGLE
-    else:
-        return 'NONE'
-
-
 df['lane'] = df.apply(lambda row: get_lane(row['role'], row['lane']), axis=1)
-df
+print(df)
 
-p = df['lane'] == MIDDLE
-
-s = requests.Session()
-s.headers.update({'X-Riot-Token': api_key})
+# Get summoner information using the Riot API
 requests.get(api_url)
-
 api_url = api_url + "?api_key=" + api_key
-
 resp = requests.get(api_url)
-
 player_info = resp.json()
-
-print(player_info)
-
 summoner_name = player_info["name"]
-
 summoner_level = player_info["summonerLevel"]
-
 print(summoner_name)
-
 print(summoner_level)
 
-json_data = resp.json()
+# Define functions for getting summoner information from the Riot API
 
 
 def get_summoner_info(summoner_name):
-    api_url = " https://euw1.api.riotgames.com/lol/summoner/v4/summoners/by-name/otto%20from%20asylum"
+    api_url = "https://euw1.api.riotgames.com/lol/summoner/v4/summoners/by-name/otto%20from%20asylum"
     api_url = api_url + "?api_key=" + api_key
     resp = requests.get(api_url)
     player_info = resp.json()
@@ -136,71 +78,4 @@ def get_account_id(summoner_name):
     account_id = player_info["accountId"]
     return account_id
 
-
-def get_summoner_info(summoner_name):
-    api_url = " https://euw1.api.riotgames.com/lol/summoner/v4/summoners/by-name/otto%20from%20asylum"
-    api_url = api_url + "?api_key=" + api_key
-    resp = requests.get(api_url)
-    player_info = resp.json()
-    return player_info
-
-
-x = get_summoner_info("otto from asylum")
-
-getsummonername = get_summoner_name("otto from asylum")
-
-
-try:
-    response = lol_watcher.summoner.by_name(
-        my_region, 'this_is_probably_not_anyones_summoner_name')
-except ApiError as err:
-    if err.response.status_code == 429:
-        print('We should retry in {} seconds.'.format(
-            err.response.headers['Retry-After']))
-        print('this retry-after is handled by default by the RiotWatcher library')
-        print('future requests wait until the retry-after time passes')
-    elif err.response.status_code == 404:
-        print('Summoner with that ridiculous name not found.')
-    else:
-        raise
-
-
-def api_request(url):
-    response = requests.get(url)
-    if response.status_code == 200:
-        return response.json()
-    else:
-        return None
-
-
-def league_request(summoner_id):
-    url = 'https://euw1.api.riotgames.com/lol/league/v4/entries/by-summoner/{}?api_key={}'.format(
-        summoner_id, api_key)
-    return api_request(url)
-
-
-try:
-    assert league_request('this_is_probably_not_anyones_summoner_id') == None
-except AssertionError:
-    print('Summoner with that ridiculous name not found.')
-
-
-def error_handling(error):
-    if error.response.status_code == 429:
-        print('We should retry in {} seconds.'.format(
-            error.response.headers['Retry-After']))
-        print('this retry-after is handled by default by the RiotWatcher library')
-        print('future requests wait until the retry-after time passes')
-    elif error.response.status_code == 404:
-        print('Summoner with that ridiculous name not found.')
-    else:
-        raise
-
-
-def league_request(summoner_id):
-    url = 'https://euw1.api.riotgames.com/lol/league/v4/entries/by-summoner/{}?api_key={}'.format(
-        summoner_id, api_key)
-    try:
-        return api_request(url)
-    except ApiError as err:
-        error_handling(err)
+# Get summoner information using the
