8ed2466e251628b5cbf5ba9bb1f323d73d4e3dfe
Otto <olarsen22@gmail.com>
2023-03-31 15:54:52 +0200

    add funcitons for getting summ info from Riot API

---
+++ main.py
@@ -0,0 +1,82 @@
+import requests
+from riotwatcher import LolWatcher, ApiError
+import pandas as pd
+
+# Initialize the API and region
+api_key = ""
+lol_watcher = LolWatcher(api_key)
+region = 'euw1'
+
+# Get the latest champion version and list of champions
+latest_version = lol_watcher.data_dragon.versions_for_region(region)[
+    'n']['champion']
+champion_data = lol_watcher.data_dragon.champions(
+    latest_version, False, 'en_US')
+
+# Get summoner information
+summoner_name = 'otto from asylum'
+summoner_info = lol_watcher.summoner.by_name(region, summoner_name)
+print(summoner_info)
+ranked_stats = lol_watcher.league.by_summoner(region, summoner_info['id'])
+print(ranked_stats)
+
+# Map the champion IDs to their names
+champ_dict = {}
+for champ_key in champion_data['data']:
+    row = champion_data['data'][champ_key]
+    champ_dict[row['key']] = row['id']
+
+# Print list of plyrs with champ names
+participants = []
+for row in participants:
+    champ_id = str(row['champion'])
+    champ_name = champ_dict.get(champ_id)
+print(champ_id + ' ' + champ_name)
+row['championName'] = champ_name
+
+# Standarize lane and role info
+df = pd.DataFrame(participants)
+df['lane'] = df.apply(lambda row: get_lane(row['role'], row['lane']), axis=1)
+print(df)
+
+# Get summoner info using Riot API
+api_url = f"https://{region}.api.riotgames.com/lol/summoner/v4/summoners/by-name/{summoner_name}"
+s = requests.Session()
+s.headers.update({'X-Riot-Token': api_key})
+resp = s.get(api_url)
+player_info = resp.json()
+summoner_name = player_info["name"]
+summoner_level = player_info["summonerLevel"]
+print(summoner_name)
+print(summoner_level)
+
+# Defining functions to allow retreival of summoner info from Riot API
+
+
+def get_summoner_info(summoner_name, region, api_key):
+    api_url = f"https://{region}.api.riotgames.com/lol/summoner/v4/summoners/by-name/{summoner_name}"
+    s = requests.Session()
+    s.headers.update({'X-Riot-Token': api_key})
+    resp = s.get(api_url)
+    player_info = resp.json()
+    return player_info
+
+
+def get_summoner_name(player_info):
+    summoner_name = player_info["name"]
+    return summoner_name
+
+
+def get_summoner_level(player_info):
+    summoner_level = player_info["summonerLevel"]
+    return summoner_level
+
+
+def get_summoner_id(player_info):
+    summoner_id = player_info["id"]
+    return summoner_id
+
+
+def get_account_id(player_info):
+    account_id = player_info["accountId"]
+    return account_id
