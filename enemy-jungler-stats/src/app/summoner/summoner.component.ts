63a8f65146d30bfcca63e5b90278df95a8f80a5c
Otto <olarsen22@gmail.com>
2023-05-01 22:52:04 +0200

    add constructor param

--- enemy-jungler-stats/src/app/summoner/summoner.component.ts
+++ enemy-jungler-stats/src/app/summoner/summoner.component.ts
@@ -8,7 +8,7 @@ import { HttpClient } from '@angular/common/http';
 })
 export class SummonerComponent implements OnInit {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
   ngOnInit(): void {
   }
