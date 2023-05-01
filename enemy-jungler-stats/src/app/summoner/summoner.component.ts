eb19652f75d363b1025ffb6684337b9652a0bd2b
Otto <olarsen22@gmail.com>
2023-05-01 22:57:19 +0200

    add onSubmit method to summoner ts file

--- enemy-jungler-stats/src/app/summoner/summoner.component.ts
+++ enemy-jungler-stats/src/app/summoner/summoner.component.ts
@@ -1,13 +1,21 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
+
+
 @Component({
   selector: 'app-summoner',
   templateUrl: './summoner.component.html',
   styleUrls: ['./summoner.component.css']
 })
-export class SummonerComponent implements OnInit {
 
+export class SummonerComponent implements OnInit {
+  
+  summoner_name: string = '';
+  region: string = '';
+  api_key: string = '';
+  summonerInfo: any;
+  
   constructor(private http: HttpClient) { }
 
   ngOnInit(): void {
@@ -16,4 +24,11 @@ export class SummonerComponent implements OnInit {
   getSummonerInfo(summoner_name: string, region: string, api_key: string) {
     return this.http.get(`http://localhost:5000/summoner?summoner_name=${summoner_name}&region=${region}&api_key=${api_key}`);
   }
+
+  onSubmit() {
+    this.getSummonerInfo(this.summoner_name, this.region, this.api_key).subscribe((data: any) => {
+      this.summonerInfo = data;
+    });
+  }
+  
 }
