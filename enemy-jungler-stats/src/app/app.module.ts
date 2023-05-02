ab00d30578a41b2bcd7f327fbe9be3bc480ad8ef
Otto <olarsen22@gmail.com>
2023-05-02 22:03:38 +0200

    add forms module to app.module

--- enemy-jungler-stats/src/app/app.module.ts
+++ enemy-jungler-stats/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SummonerComponent } from './summoner/summoner.component';
@@ -12,7 +12,8 @@ import { SummonerComponent } from './summoner/summoner.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
