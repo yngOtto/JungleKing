894e41d2c323d6bbbdf9497af394134ff14d6dbf
Otto <olarsen22@gmail.com>
2023-05-03 13:05:03 +0200

    add get_summoner_info method

--- riot_api.py
+++ riot_api.py
@@ -1,5 +1,29 @@
 import requests
 from riotwatcher import LolWatcher
 
-import requests
-from riotwatcher import LolWatcher
+
+class RiotAPI:
+    def __init__(self, api_key, region):
+        self.api_key = api_key
+        self.region = region
+        self.lol_watcher = LolWatcher(api_key)
+
+    def get_summoner_info(self, summoner_name):
+        api_url = f"https://{self.region}.api.riotgames.com/lol/summoner/v4/summoners/by-name/{summoner_name}"
+        s = requests.Session()
+        s.headers.update({'X-Riot-Token': self.api_key})
+        resp = s.get(api_url)
+        player_info = resp.json()
+        return player_info
+
+    # todo lisy:
+    # - get_summoner_name()
+    # - get_summoner_level()
+    # - get_summoner_id()
+    # - get_account_id()
+    # - get_match_history()
+    # - get_match_info_jungler()
+    # - get_match_info()
+    # - get_win_rate()
+    # - get_match_info_jungler()
+    # - analyze_enemy_jungler_pathing()
