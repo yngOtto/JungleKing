69abdb951bd07e4193143764e4029dc8bbab6fbb
Otto <olarsen22@gmail.com>
2023-05-03 12:57:54 +0200

    add get_api_key method

--- config.py
+++ config.py
@@ -2,5 +2,7 @@ import configparser
 
 
 def get_api_key(config_file='config.ini'):
-
+    config = configparser.ConfigParser()
+    config.read(config_file)
+    api_key = config['RIOT']['api_key']
     return api_key
